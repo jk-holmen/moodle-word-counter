f168fd8e6c49360210eea24cf226509c0b1d83f6
jk-holmen <166861650+jk-holmen@users.noreply.github.com>
2025-11-16 20:08:16 +0100

    Refactor CLI script to improve input handling and path validation

--- mdl_count.py
+++ mdl_count.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env -S uv run --script
+#!/usr/bin/env
 #
 # /// script
 # requires-python = ">=3.12"
-# dependencies = ["bs4"]
+# dependencies = ["lxml", "bs4"]
 # ///
 
 import argparse
 import html
 import os
 import re
+from pathlib import Path
 
 from bs4 import BeautifulSoup
 
@@ -16,75 +17,95 @@ OUTPUT_FILE_PATH = "./course_out.txt"
 CLASSES_TO_STRIP = ["anh-reading"]  # don't include references
 
 
-def get_input_dir():
-    parser = argparse.ArgumentParser(description="Set input and output directories")
+class WordCounter:
+    def __init__(self):
+        args = self.get_args()
+        self.course = Course(args.input)
 
-    parser.add_argument(
-        "-i", "--input", type=str, required=True, help="Path to the input backup file"
-    )
-
-    args = parser.parse_args()
-    input_file_path = args.input
-
-    if not os.path.isfile(input_file_path):
-        raise ValueError(f"Input backup file not found in: {input_file_path}")
-    if not os.path.splitext(input_file_path)[-1] == ".mbz":
-        raise ValueError(
-            f"File specified does not have an .mbz extension: {input_file_path}"
+    def get_args(self):
+        parser = argparse.ArgumentParser(
+            description="Get the word count of a Moodle course from a backup file"
         )
 
-    return input_file_path
+        parser.add_argument(
+            "-i",
+            "--input",
+            type=str,
+            required=True,
+            help="Path to the input backup file",
+        )
+
+        return parser.parse_args()
 
 
-def extract_content(file_path, *tags):
-    with open(file_path, "r", encoding="utf-8") as file:
-        soup = BeautifulSoup(file.read(), "xml")
+class Course:
+    def __init__(self, input_path):
+        self.input_path = Path(input_path)
+        self.validate_path()
+
+    def validate_path(self):
+        if not self.input_path.is_absolute():
+            raise ValueError(f"Given file path is not absolute: {self.input_path}")
+        # if not os.path.isfile(self.input_path):
+        #    raise ValueError(f"Input backup file not found in: {self.input_path}")
+        # if not os.path.splitext(self.input_path)[-1] == ".mbz":
+        #    raise ValueError(
+        #        f"File specified does not have an .mbz extension: {self.input_path}"
+        #    )
+
+    def extract_content(self, file_path, *tags):
+        with open(file_path, "r", encoding="utf-8") as file:
+            soup = BeautifulSoup(file.read(), "xml")
+            content = ""
+            for tag in tags:
+                content += self.extract_per_tag(soup, tag)
+            return content
+
+    # For each xml tag extracted, unescape and parse the html,
+    # remove divs with skipped classes, then extract only the text
+    def extract_per_tag(self, soup, tag_name):
+        tags = soup.find_all(tag_name)
         content = ""
-        for tag in tags:
-            content += extract_per_tag(soup, tag)
+        for _, tag_content in enumerate(tags):
+            decoded_html = html.unescape(tag_content.text)
+            html_soup = BeautifulSoup(decoded_html, "html.parser")
+
+            for cls in CLASSES_TO_STRIP:
+                for div in html_soup.find_all("div", class_=cls):
+                    div.decompose()
+
+            content += html_soup.get_text() + "\n"
         return content
 
 
-# For each xml tag extracted, unescape and parse the html,
-# remove divs with skipped classes, then extract only the text
-def extract_per_tag(soup, tag_name):
-    tags = soup.find_all(tag_name)
-    content = ""
-    for _, tag_content in enumerate(tags):
-        decoded_html = html.unescape(tag_content.text)
-        html_soup = BeautifulSoup(decoded_html, "html.parser")
-
-        for cls in CLASSES_TO_STRIP:
-            for div in html_soup.find_all("div", class_=cls):
-                div.decompose()
-
-        content += html_soup.get_text() + "\n"
-    return content
-
-
 def main():
-    input_file_path = get_input_dir()
+    counter = WordCounter()
+
+    path = counter.course.input_path
+
     content = ""
 
-    for _root, dirs, _files in os.walk(input_file_path + "/activities/"):
+    for _root, dirs, _files in os.walk(path / "activities"):
         for dir_name in dirs:
             if "page_" in dir_name:
-                file_path = input_file_path + "/activities/" + dir_name + "/page.xml"
+                file_path = path / "activities" / dir_name / "page.xml"
                 print(file_path)
-                content += extract_content(file_path, "name", "intro", "content")
+                content += counter.course.extract_content(
+                    file_path, "name", "intro", "content"
+                )
             if "book_" in dir_name:
-                file_path = input_file_path + "/activities/" + dir_name + "/book.xml"
+                file_path = path / "activities" / dir_name / "book.xml"
                 print(file_path)
-                content += extract_content(
+                content += counter.course.extract_content(
                     file_path, "name", "title", "intro", "content"
                 )
 
-    for _root, dirs, _files in os.walk(input_file_path + "/sections/"):
+    for _root, dirs, _files in os.walk(path / "sections"):
         for dir_name in dirs:
             if "section_" in dir_name:
-                file_path = input_file_path + "/sections/" + dir_name + "/section.xml"
+                file_path = path / "sections" / dir_name / "section.xml"
                 print(file_path)
-                content += extract_content(file_path, "name", "summary")
+                content += counter.course.extract_content(file_path, "name", "summary")
 
     content = content.replace("\u00a0", "")  # Remove non-breaking spaces
     content = re.sub(r"@@.*", "", content)  # Remove plugin annotations
